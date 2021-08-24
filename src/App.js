477def75496569766f53f87b1230359f81d2b51f
jinseongFBI <wlstjddydrl5@gmail.com>
2021-08-24 15:34:28 +0900

    styled-components-example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import StyledButton from './components/StyledButton';
+import styled, { createGlobalStyle } from 'styled-components';
+import StyledA from "./components/StyledA"
+const PrimaryStyledButton = styled(StyledButton)`
+background: palevioletred;
+color: white;
+`;
+const UppercaseButton = (props) =><button {...props} children={props.children.toUpperCase()}/>
+
+const MyButton = (props) =>
+<button className={props.className} children={`MyButton${props.children}`}/>
+
+const StyledMyButton= styled(MyButton)`
+background: transparent;
+border-radius:3px;
+border:2px solid ${(props)=>props.color || "palevioltred"};
+color: ${(props)=>props.color || "palevioltred"};
+margin: 0 1em;
+padding: 0.25em 1em;
+font-size:20px
+:hover{
+  border: 2px solid red;
+}
+::before {
+  contact: "@";
+}
+`;
+const GlobalStyle= createGlobalStyle`
+button{
+  color:yellow;
+}`;
+
+
 
 function App() {
   return (
@@ -7,16 +40,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <StyledButton>버튼</StyledButton>
+         <StyledButton primary>버튼</StyledButton>
+         <PrimaryStyledButton >버튼</PrimaryStyledButton>
+         <StyledButton  as="a" href="/">
+           버튼
+           </StyledButton>
+         <StyledButton as ={UppercaseButton}>button</StyledButton>
+         
+         <StyledMyButton>button</StyledMyButton>
+         <StyledMyButton color="green">button</StyledMyButton>
+         <StyledA href="https://google.com" >태그</StyledA>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
---
+++ src/components/StyledButton.jsx
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components";
+
+const StyledButton = styled.button`
+background: transparent;
+border-radius:3px;
+border:2px solid palevioletred;
+color:white;
+margin: 0 1em;
+padding: 0.25em 1em;
+font-size:20px
+
+${props=>props.primary &&
+   css`
+    background: palevioletred;
+    color: white;`}
+`;
+
+export default StyledButton;
